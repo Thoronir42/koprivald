e957201fcf346fe59d214622d7ea3c16f3f4226d
Štěpán Śevčík <Thoronir42@Gmail.com>
2021-02-27 12:01:25 +0100

    Add egg AJ

--- app/routes.ts
+++ app/routes.ts
@@ -10,8 +10,14 @@ routes.get('/about-us', (req, res) => {
     res.render('aboutUs')
 })
 
-routes.get('/who', (req, res) => {
-    res.send('Meeee');
+routes.get('/eggs/aj', (req, res) => {
+    let params = {
+        secretUnlocked: false,
+    };
+    if (req.query.secret === 'anezka') {
+        params.secretUnlocked = true;
+    }
+    res.render('eggs/aj', params);
 })
 
 export default routes;
